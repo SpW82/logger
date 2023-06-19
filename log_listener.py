15cef4c6d559f9b49dd11473b487dfc00ffbd3ff
Shane Whelan <41593034+SpW82@users.noreply.github.com>
2023-06-19 23:04:16 +0100

    Update log_listener.py

--- log_listener.py
+++ log_listener.py
@@ -1,12 +1,15 @@
+#!/usr/bin/python
+
 from socket import *
 from termcolor import colored as col
 from datetime import datetime
+import optparse
 
 """
 Listens on specified port for an incoming connection, when received the sent data is logged locally.
-This script is the first part of a two part program, log_client being the second.
-Listening port is hardcoded in to match the client.
-Uncomment line 37 for user defined port but also change the client port.
+This script is the first part of a two part program, logger_client_parser2.py being the second.
+Use the -p or --prt flag followed by the desired port number to set a port.
+If no port is specified, the server will run on 12345.
 The default settings will run locally.
 """
 
@@ -27,17 +30,24 @@ class Server:
                 while True:
                     c, a = s.accept()
                     k = c.recv(1024).decode()
-
                     with open('logd.txt', 'a') as f:
                         f.write(f"{datetime.now()}: {k}\n")
 
             except KeyboardInterrupt:
 
-                print(f"{col('[*]', 'yellow')} Server shutting down")
+                print(f"\n{col('[*]', 'yellow')} Server shutting down")
 
 def main():
-    port = 5432
-    # port = input("[*] Enter port number to listen on: ")
+    parser = optparse.OptionParser('A simple server to log received data to a file')
+    parser.add_option('-p','--prt', dest='prt', type='int', help='port to listen for connection (12345 is default)')
+    (options, args) = parser.parse_args()
+    port = 12345
+
+    if options.prt is None:
+        pass
+    else:
+        port = options.prt
+
     server = Server(port)
     server.start()
 
